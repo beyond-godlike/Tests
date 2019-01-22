69cace339a301f6c6cd4e700146f97674d29f915
beyond-godlike <dia_the_ghost@mail.ru>
2019-01-22 02:25:51 +0200

    bugs fixed

--- app/src/main/java/com/example/dia/tests/ui/TestActivity.kt
+++ app/src/main/java/com/example/dia/tests/ui/TestActivity.kt
@@ -2,28 +2,30 @@ package com.example.dia.tests.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import com.example.dia.tests.R
 import kotlinx.android.synthetic.main.activity_test.*
+
+
 
 class TestActivity : AppCompatActivity() {
     var presenter: Presenter? = null
 
     private var filename = "v1.xml" // default
 
-    private var currentQuestion: Int = 0
+    private var counter: Int = 0
     private var correctCounter: Int = 0
     private var pressedButtonId: Int = 0
     private var pressed: Boolean = false
+    private var ended: Boolean = false
 
     // SAVINGS
-    private val CURRENT_QUESTION: String = "CURRENT_QUESTION"
-    private val CORRECT_COUNTER: String = "CORRECT_COUNTER"
-    private val PRESSED_BUTTON_ID: String = "PRESSED_BUTTON_ID"
-    private val PRESSED_STATE: String = "PRESSED_STATE"
+    private val currentQuestionStr: String = "CURRENT_QUESTION"
+    private val correctCounterStr: String = "CORRECT_COUNTER"
+    private val pressedButtonIdStr: String = "PRESSED_BUTTON_ID"
+    private val pressedStateStr: String = "PRESSED_STATE"
+    private val endedTestStr: String = "ENDED_TEST"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +36,7 @@ class TestActivity : AppCompatActivity() {
 
         if(savedInstanceState != null)
         {
-            currentQuestion = savedInstanceState!!.getInt(CURRENT_QUESTION)
-            correctCounter = savedInstanceState!!.getInt(CORRECT_COUNTER)
-            pressedButtonId = savedInstanceState!!.getInt(PRESSED_BUTTON_ID)
-            pressed = savedInstanceState!!.getBoolean(PRESSED_STATE)
-            restore()
+            restore(savedInstanceState)
         }
         else {
             onStartFirst()
@@ -46,19 +44,19 @@ class TestActivity : AppCompatActivity() {
 
         // вешаем лиснеры
         buttonAnswer1.setOnClickListener {
-            save(it.id, buttonAnswer1.text.toString())
+            save(it.id)
             colorButtons(it.id)
         }
         buttonAnswer2.setOnClickListener {
-            save(it.id, buttonAnswer2.text.toString())
+            save(it.id)
             colorButtons(it.id)
         }
         buttonAnswer3.setOnClickListener {
-            save(it.id, buttonAnswer3.text.toString())
+            save(it.id)
             colorButtons(it.id)
         }
         buttonAnswer4.setOnClickListener {
-            save(it.id, buttonAnswer4.text.toString())
+            save(it.id)
             colorButtons(it.id)
         }
         buttonNext.setOnClickListener {
@@ -69,24 +67,25 @@ class TestActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        outState!!.putInt(CURRENT_QUESTION, currentQuestion)
-        outState!!.putInt(CORRECT_COUNTER, correctCounter)
-        //outState!!.putString("CURRENT_ANSWER", currentAnswer) //?
-        outState!!.putInt(PRESSED_BUTTON_ID, pressedButtonId)
-        outState!!.putBoolean(PRESSED_STATE, pressed)
+        outState!!.putInt(currentQuestionStr, counter)
+        outState!!.putInt(correctCounterStr, correctCounter)
+        outState!!.putInt(pressedButtonIdStr, pressedButtonId)
+        outState!!.putBoolean(pressedStateStr, pressed)
+        outState!!.putBoolean(endedTestStr, ended)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
 
-        currentQuestion = savedInstanceState!!.getInt(CURRENT_QUESTION)
-        correctCounter = savedInstanceState!!.getInt(CORRECT_COUNTER)
-        pressedButtonId = savedInstanceState!!.getInt(PRESSED_BUTTON_ID)
-        pressed = savedInstanceState!!.getBoolean(PRESSED_STATE)
+        counter = savedInstanceState!!.getInt(currentQuestionStr)
+        correctCounter = savedInstanceState!!.getInt(correctCounterStr)
+        pressedButtonId = savedInstanceState!!.getInt(pressedButtonIdStr)
+        pressed = savedInstanceState!!.getBoolean(pressedStateStr)
+        ended = savedInstanceState!!.getBoolean(endedTestStr)
     }
 
     // click performed
-    private fun save(id: Int, textFromBtn: String) {
+    private fun save(id: Int) {
         pressedButtonId = id
         pressed = true
 
@@ -132,7 +131,7 @@ class TestActivity : AppCompatActivity() {
         // теперь нужно покрасить правильный ответ
         // хотя эту кнопку мы не нажимали
         //currentQuestion-1
-        when(presenter!!.itemList[currentQuestion-1].correct) {
+        when(presenter!!.itemList[counter].correct) {
             buttonAnswer1.text.toString() -> buttonAnswer1.setBackgroundResource(R.drawable.button_style_try_correct)
             buttonAnswer2.text.toString() -> buttonAnswer2.setBackgroundResource(R.drawable.button_style_try_correct)
             buttonAnswer3.text.toString() -> buttonAnswer3.setBackgroundResource(R.drawable.button_style_try_correct)
@@ -150,6 +149,29 @@ class TestActivity : AppCompatActivity() {
 
     }
 
+    private fun loadQuestionsIntoButtons() {
+        if(!ended) {
+            // refresh button colors
+            colorClearButtons()
+
+            textView.text = presenter!!.itemList?.get(counter)?.question
+            buttonAnswer1.text = presenter!!.itemList[counter].answers[0]
+            buttonAnswer2.text = presenter!!.itemList[counter].answers[1]
+            buttonAnswer3.text = presenter!!.itemList[counter].answers[2]
+            buttonAnswer4.text = presenter!!.itemList[counter].answers[3]
+        }
+        else {
+            // TODO show results
+            buttonAnswer1.visibility = View.INVISIBLE
+            buttonAnswer2.visibility = View.INVISIBLE
+            buttonAnswer3.visibility = View.INVISIBLE
+            buttonAnswer4.visibility = View.INVISIBLE
+            buttonNext.visibility = View.INVISIBLE
+
+            textView.text = "Total: " +  correctCounter.toString() + " from " + presenter!!.totalQuestions.toString()
+            ended = true
+        }
+    }
     private fun loadNextQuestion() {
         // unlock buttons if was pressed
         if(pressed) {
@@ -162,44 +184,33 @@ class TestActivity : AppCompatActivity() {
         else {
 
         }
-
         pressed = false
-        if (presenter!!.totalQuestions != currentQuestion)
+        // refresh button colors
+        colorClearButtons()
+
+        //(itemList.size()-1) > counter
+        //if (presenter!!.totalQuestions != currentQuestion)
+        //Log.d("ssss", presenter!!.itemList!!.size.toString())
+        //Log.d("ssss", counter.toString())
+        if((presenter!!.itemList!!.size - 1) > counter)
         {
-            // refresh button colors
-            colorClearButtons()
-            textView.text = presenter!!.itemList?.get(currentQuestion)?.question
-            buttonAnswer1.text = presenter!!.itemList.get(currentQuestion).answers.get(0)
-            buttonAnswer2.text = presenter!!.itemList.get(currentQuestion).answers.get(1)
-            buttonAnswer3.text = presenter!!.itemList.get(currentQuestion).answers.get(2)
-            buttonAnswer4.text = presenter!!.itemList.get(currentQuestion).answers.get(3)
-
-            currentQuestion++
-        }
-        else {
-            // TODO show results
-            buttonAnswer1.visibility = View.INVISIBLE
-            buttonAnswer2.visibility = View.INVISIBLE
-            buttonAnswer3.visibility = View.INVISIBLE
-            buttonAnswer4.visibility = View.INVISIBLE
-            buttonNext.visibility = View.INVISIBLE
-
-            textView.text = "ToTal: " +  correctCounter.toString() + " from " + presenter!!.totalQuestions.toString()
+            counter++
+            loadQuestionsIntoButtons()
         }
     }
 
     private fun checkAnswer(textFromButton: String): Boolean {
         // ответ правильный
-        Log.d("dddd", textFromButton) // real
+        //Log.d("dddd", textFromButton) // real
         //Log.d("dddd", presenter!!.itemList[currentQuestion-1].correct)
 
-        if(textFromButton == presenter!!.itemList.get(currentQuestion-1).correct) {
+        return if(textFromButton == presenter!!.itemList[counter].correct) {
             correctCounter++
-            return true
+            true
         }
         // ответ не правильный
         else {
-            return false
+            false
         }
     }
 
@@ -207,18 +218,31 @@ class TestActivity : AppCompatActivity() {
         presenter = Presenter(this.applicationContext)
         presenter!!.doParse(filename)
 
-        loadNextQuestion()
+        loadQuestionsIntoButtons()
     }
 
-    private fun restore() {
+    private fun restore(bundle: Bundle?) {
+        counter = bundle!!.getInt(currentQuestionStr)
+        correctCounter = bundle!!.getInt(correctCounterStr)
+        pressedButtonId = bundle!!.getInt(pressedButtonIdStr)
+        pressed = bundle!!.getBoolean(pressedStateStr)
+        ended = bundle!!.getBoolean(endedTestStr)
+
         presenter = Presenter(this.applicationContext)
         presenter!!.doParse(filename)
 
         //currentQuestion
-        loadNextQuestion()
-        if(pressed)
+        //loadNextQuestion()
+        loadQuestionsIntoButtons()
+        if(pressed) // emit click
         {
-            colorButtons(pressedButtonId)
+            when(pressedButtonId)
+            {
+                buttonAnswer1.id -> buttonAnswer1.post { buttonAnswer1.performClick() }
+                buttonAnswer2.id -> buttonAnswer2.post { buttonAnswer2.performClick() }
+                buttonAnswer3.id -> buttonAnswer3.post { buttonAnswer3.performClick() }
+                buttonAnswer4.id -> buttonAnswer4.post { buttonAnswer4.performClick() }
+            }
         }
 
     }
